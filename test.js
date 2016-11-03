0058e439517f1a07e9d2ea641f572a90ed83ed82
Jannis R <mail@jannisr.de>
2016-11-03 10:28:33 +0100

    regression test :white_check_mark:

---
+++ test.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const fs = require('fs')
+const path = require('path')
+const h = require('virtual-hyperscript-svg')
+const toHTML = require('virtual-dom-stringify')
+const disparity = require('disparity')
+
+const map = require('.')
+const fixture = fs.readFileSync(path.join(__dirname, 'example/berlin.svg'), 'utf8')
+
+const assertEqualString = (a, b) => {
+	if (a === b) return
+	process.stdout.write(disparity.chars(a, b) + '\n')
+	process.exit(1)
+}
+
+
+
+const generated = toHTML(map(13.34, 52.54))
+
+assertEqualString(generated, fixture)
