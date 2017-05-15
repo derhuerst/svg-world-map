baac3dc2271c295f418d9a57a2deb4a570b515f6
Jannis R <mail@jannisr.de>
2017-05-15 15:29:57 +0200

    round -> toFixed, update fixture, docs :memo:

--- projection.js
+++ projection.js
@@ -2,12 +2,12 @@
 
 const mercator = require('projections/mercator')
 
-const round = (x, n = 5) =>
-	Math.round(x * Math.pow(10, n)) / Math.pow(10, n)
-
 const projection = ([lon, lat]) => {
 	const {x, y} = mercator({lon, lat}, {latLimit: 80})
-	return [round(x * 100, 3), round(y * 100, 3)]
+	return [
+		+(x * 100).toFixed(3),
+		+(y * 100).toFixed(3)
+	]
 }
 
 module.exports = projection
